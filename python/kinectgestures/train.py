59523a17d3d3583607195d2338a8602a3effb4cb
MarcoLengua <marcoantonio.lengua@gmail.com>
2019-08-12 11:02:47 +0200

    save score in history dict

--- python/kinectgestures/train.py
+++ python/kinectgestures/train.py
@@ -2,10 +2,9 @@ import os
 
 import numpy as np
 
-# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID";
-#
-# # The GPU id to use, usually either "0" or "1";
-# os.environ["CUDA_VISIBLE_DEVICES"] = "1";
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID";
+# The GPU id to use, usually either "0" or "1";
+os.environ["CUDA_VISIBLE_DEVICES"] = "1";
 import keras
 from keras.callbacks import ModelCheckpoint
 
