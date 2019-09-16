f5c735edb26d9fdff0ec70165c75278ee7e400f1
Marco Antonio Lengua Hinojosa <1lengua@informatik.uni-hamburg.de>
2019-09-16 15:35:05 +0200

    gpu device order out commented

--- python/kinectgestures/train.py
+++ python/kinectgestures/train.py
@@ -2,9 +2,9 @@ import os
 
 import numpy as np
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID";
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID";
 # The GPU id to use, usually either "0" or "1";
-os.environ["CUDA_VISIBLE_DEVICES"] = "1";
+#os.environ["CUDA_VISIBLE_DEVICES"] = "1";
 import keras
 #from keras import backend as K
 #K.tensorflow_backend._get_available_gpus()
