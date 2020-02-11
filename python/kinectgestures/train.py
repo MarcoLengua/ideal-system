f55963d4a1c04de1c9b6d686b27717db0f4b1afa
MarcoLengua <marcoantonio.lengua@gmail.com>
2020-02-11 14:39:27 +0100

    cuda device order for titanrtx

--- python/kinectgestures/train.py
+++ python/kinectgestures/train.py
@@ -1,9 +1,9 @@
 import os
 import numpy as np
 
-#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID";
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID";
 # The GPU id to use, usually either "0" or "1";
-#os.environ["CUDA_VISIBLE_DEVICES"] = "1";
+os.environ["CUDA_VISIBLE_DEVICES"] = "1";
 
 from keras.callbacks import ModelCheckpoint
 import keras
